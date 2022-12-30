4e78b1a92680a0c5c5d7e69124d216f14acc76de
Snehit Sah <snehitsah@protonmail.com>
2022-12-30 12:21:27 +0530

    feat: add package card
    Signed-off-by: Snehit Sah <snehitsah@protonmail.com>

--- lib/pages/page_home.dart
+++ lib/pages/page_home.dart
@@ -6,6 +6,12 @@ import 'package:paxyhub/components/components.dart';
 const searchLabel = "Search for Packages";
 const searchInputHint = "Enter a package name";
 const featuredText = "Featured packages";
+const popularText = "Popular packages";
+const recentText = "Recently updated packages";
+
+// dummy data
+var listPackagesTitle = List<String>.generate(10, (i) => 'Package $i');
+var listPackagesInfo = List<String>.generate(10, (i) => 'Info for package $i');
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -68,6 +74,73 @@ class HomePage extends StatelessWidget {
                       ),
                     ),
                   ),
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Card(
+                        elevation: 3,
+                        child: Container(
+                          width: 250,
+                          height: 120,
+                          padding: EdgeInsets.all(10.0),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.all(12.0),
+                                child: Icon(Icons.archive),
+                              ),
+                              Flexible(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Align(
+                                      alignment: Alignment.centerLeft,
+                                      child: SelectableText(
+                                        "Package 1",
+                                        style: headlineSecondaryTextStyle,
+                                      ),
+                                    ),
+                                    SizedBox(height: 4),
+                                    Text(
+                                      "This is a package description. This package helps you get your work done.",
+                                      overflow: TextOverflow.clip,
+                                      maxLines: 3,
+                                      style: subtitleTextStyle,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  divider,
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Container(
+                      margin: marginBottom12,
+                      child: SelectableText(
+                        popularText,
+                        style: headlineTextStyle,
+                      ),
+                    ),
+                  ),
+                  divider,
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Container(
+                      margin: marginBottom12,
+                      child: SelectableText(
+                        recentText,
+                        style: headlineTextStyle,
+                      ),
+                    ),
+                  ),
+                  divider,
                   const Footer(),
                 ],
               ),
